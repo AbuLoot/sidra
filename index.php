8f8421cf30198452c41f70bd865ac6253c81f5e4
Adl <issayev.adilet@gmail.com>
2018-09-20 17:03:53 +0600

    added metrics

--- index.php
+++ index.php
@@ -902,6 +902,41 @@
 
   <!-- <a id="backtotop" class="scrollup scrollup--fixed"><i class="fa fa-angle-up"></i></a> -->
 
+  <!-- Yandex.Metrika informer -->
+  <a href="https://metrika.yandex.kz/stat/?id=50420026&amp;from=informer"
+  target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/50420026/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
+  style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class="ym-advanced-informer" data-cid="50420026" data-lang="ru" /></a>
+  <!-- /Yandex.Metrika informer -->
+
+  <!-- Yandex.Metrika counter -->
+  <script type="text/javascript" >
+      (function (d, w, c) {
+          (w[c] = w[c] || []).push(function() {
+              try {
+                  w.yaCounter50420026 = new Ya.Metrika2({
+                      id:50420026,
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true,
+                      webvisor:true
+                  });
+              } catch(e) { }
+          });
+
+          var n = d.getElementsByTagName("script")[0],
+              s = d.createElement("script"),
+              f = function () { n.parentNode.insertBefore(s, n); };
+          s.type = "text/javascript";
+          s.async = true;
+          s.src = "https://mc.yandex.ru/metrika/tag.js";
+
+          if (w.opera == "[object Opera]") {
+              d.addEventListener("DOMContentLoaded", f, false);
+          } else { f(); }
+      })(document, window, "yandex_metrika_callbacks2");
+  </script>
+  <noscript><div><img src="https://mc.yandex.ru/watch/50420026" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+  <!-- /Yandex.Metrika counter -->
   <!-- LOAD JQUERY LIBRARY -->
   <script src="/js/jquery.min.js"></script>
   <script src="/js/jquery-migrate.min.js"></script>
